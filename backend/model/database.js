0808c866c443ce9b8527fcc0a47d105548724ca6
Harshavardhanan <harshavardhan2990@gmail.com>
2019-01-27 13:32:50 +0530

    Mongo actions created

--- backend/model/database.js
+++ backend/model/database.js
@@ -10,22 +10,58 @@ mongoose.connect(db_details, { useNewUrlParser: true }, (err) => {
     }
 });
 
-let newTodo = new Todo({
-    todo: "Drink Booze",
-    timestamp: new Date(),
-    isCompleted: false
-})
-newTodo.save((err) => {
-    if (err)
-        console.log(err);
-    else {
-        Todo.find({ "todo": "Drink Booze" }, (err, todo) => {
-            if (err)
-                console.log(err);
-            else
-                console.log(todo);
-        })
-    }
-    console.log("Inserted");
-});
+export function addTodo(todo, createdDate, isCompleted) {
+    let newTodo = new Todo({
+        todo: todo,
+        timestamp: createdDate,
+        isCompleted: isCompleted
+    });
 
+    newTodo.save((err) => {
+        if (err) {
+            console.log("Error inserting todo:")
+            console.log(err);
+        } else
+            console.log("Inserted todo into db");
+    });
+}
+
+export function getTodos() {
+    Todo.find({}, (err, todos) => {
+        if (err) {
+            console.log("Error getting list of todos:")
+            console.log(err);
+        } else {
+            return todos;
+        }
+    });
+}
+
+export function getTodoByID(id) {
+    Todo.find(id, (err, todo) => {
+        if (err) {
+            console.log("Error getting todo with id => " + id);
+            console.log(err);
+        } else {
+            return todo;
+        }
+    })
+}
+
+export function updateTodo(id, isCompleted) {
+    Todo.findByIdAndUpdate(id, { isCompleted: isCompleted }, (err) => {
+        if (err) {
+            console.log("Error updating todo with id => " + id);
+            console.log(err);
+        }
+    });
+}
+
+export function deleteTodo(id) {
+    Todo.findByIdAndDelete(id, (err) => {
+        if (err) {
+            console.log("Error updating todo with id => " + id);
+            console.log(err);
+        }
+    })
+}
---
+++ backend/model/temp.js
@@ -0,0 +1,4 @@
+import * as db from './database';
+
+//db.addTodo("buy milk", new Date(), false);
+//
