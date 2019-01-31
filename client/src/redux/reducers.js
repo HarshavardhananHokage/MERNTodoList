9fcef28493f011d5f7e4786d72a44582e0959c20
Harshavardhanan <harshavardhan2990@gmail.com>
2019-01-31 08:18:09 +0530

    Completed Local Redux Change

--- client/src/redux/reducers.js
+++ client/src/redux/reducers.js
@@ -5,11 +5,26 @@ const initialState = {
 }
 
 function rootReducer(state = initialState, action) {
-    switch(action.type) {
-        case actions.GET_TODOS:
-            return Object.assign({}, state, { todos: action.todos});
-        default:
-            return state;
+
+    if(action.type === actions.GET_TODOS) {
+        return Object.assign({}, state, { todos: action.todos });
+    } else if (action.type === actions.UPDATE_TODO) {
+        let newTodos = state.todos.map((todo) => {
+            if (todo.id === action.id) {
+                 return Object.assign({}, todo, { isCompleted: !todo.isCompleted });
+            }
+            return todo;
+        });
+        return Object.assign({}, state, { todos: newTodos });
+    } else if (action.type === actions.DELETE_TODO) {
+        let newTodos = state.todos.filter((todo) => todo.id !== action.id);
+        return Object.assign({}, state, { todos: newTodos });
+    } else if (action.type === actions.ADD_TODO) {
+        let newTodos = [...state.todos, action.todo];
+        return Object.assign({}, state, { todos: newTodos });
+    }
+    else {
+        return state;
     }
 }
 
